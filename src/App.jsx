cf5a4b01486665579ebb75ca1188e7865ac2fc37
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-10-24 00:30:39 -0300

    trabaje en la implementacion de fetch que trae las canciones al reproductor y avance en la UI del reproductor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
  
 
     const [listItems, setListItems] = useState([])
+    const [ number, setNumber] = useState('block')
+    const [ playIcon, setPlayIcon] = useState('none')
     
 
     fetch("https://playground.4geeks.com//apis/fake/sound/songs")
@@ -19,19 +21,31 @@ const App = () => {
 
     })
 
+
+
   return (
     <>
       <div className="contenedorReproductor">
             <div className="controlesDelReproductor">controles</div>
+            <div className="overflowList">
             <div className="containerList">
                 <ul>
                     { 
                         listItems.length > 0 ? listItems.map((item, i)=>{
-                        return <li key={i}> <div className="itemInfo"> <p> {i + 1} </p> <p>{item.name.charAt(0).toUpperCase()+item.name.slice(1)} </p> </div> <div className="playSong">▶</div> </li> }) 
+                        return <li key={i} onMouseEnter={()=>{setPlayIcon('block'),setNumber('none')}} onMouseLeave={()=>{setPlayIcon('none'),setNumber('block')}} >
+                            <div style={{display:playIcon}}> <div className="playIconContainer" >▶</div>  </div>
+                            <div className="itemInfo"> 
+                            <div style={{display:number}}> <div className='numberContainer'>{i + 1}</div></div> 
+                            <div>{item.name.charAt(0).toUpperCase()+item.name.slice(1)}</div> 
+                            </div>  
+                            <p>0:00</p>
+                            </li> }) 
                         : 
                         "tu lista esta vacia" 
                     }
                 </ul>
+            </div>
+
             </div>
       </div>
     </>
