416ce7a3e9c23c2f23c02891c964da4c54c5ad98
Rodrigo Solis <rodrigo.solis.g@gmail.com>
2023-11-01 00:26:06 -0300

    ya pude reproducir los items de forma individual

--- src/App.jsx
+++ src/App.jsx
@@ -1,137 +1,106 @@
-import { useState, useRef, useEffect } from 'react'
-import { FaPlay, FaForwardStep, FaBackwardStep,FaPause} from "react-icons/fa6";
+import React, { useState, useRef, useEffect } from 'react';
+import { FaPlay, FaForwardStep, FaBackwardStep, FaPause } from 'react-icons/fa6';
 
 const App = () => {
-   
-  const [iconPlay, setIconPlay] = useState('block')
-  const [iconPause, setIconPause] = useState('none')
-  const [listItems, setListItems] = useState([])
-  const [ number, setNumber] = useState('block')
-  const [ playIcon, setPlayIcon] = useState('none')
-  const [ songTime, setSongTime] = useState(0)
-  const [ activeIcon, setActiveIcon ] = useState(null)
+  const [iconPlay, setIconPlay] = useState('block');
+  const [iconPause, setIconPause] = useState('none');
+  const [number, setNumber] = useState('block');
+  const [playIcon, setPlayIcon] = useState('none');
+  const [activeIcon, setActiveIcon] = useState(null);
 
-  const audioRef = useRef()
+  const audioRefs = useRef([]);
+  const [songList, setSongList] = useState([]);
+  const [activeSongIndex, setActiveSongIndex] = useState(null);
+  const [songTime, setSongTime] = useState('0:00');
 
-  useEffect(()=>{
-    fetch("https://playground.4geeks.com//apis/fake/sound/songs")
-    .then((response)=>{return response.json()})
-    .then((data)=>{setListItems(data)})
-    .catch((error)=>{console.log('Hubo un error')})
-  },[])
+  useEffect(() => {
+    fetch('https://playground.4geeks.com//apis/fake/sound/songs')
+      .then((response) => response.json())
+      .then((data) => {
+        setSongList(data);
+        audioRefs.current = data.map(() => React.createRef());
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-  useEffect(()=>{
-      setTimeout(() => {        
-        setSongTime(audioRef.current.currentTime)
-        console.log(audioRef.current.currentTime)
-        console.log(audioRef.current.duration)
-      }, 1000);
-   
-  },[])
+  const handlePlaySong = (index) => {
+    if (audioRefs.current[index].current) {
+      if (activeSongIndex !== null) {
+        audioRefs.current[activeSongIndex].current.pause();
+      }
+      audioRefs.current[index].current.play();
+      setActiveSongIndex(index);
+      setIconPlay('none');
+      setIconPause('block');
 
-  const reproducirCancion = () => {
-    setIconPause('block')
-    setIconPlay('none')
-    audioRef.current.play()
-  }
-  const pausarCancion = () => {
-    setIconPause('none')
-    setIconPlay('block')
-    audioRef.current.pause()
-  }
+      audioRefs.current[index].current.addEventListener('timeupdate', updateTime);
+    }
+  };
 
-  
+  const updateTime = () => {
+    if (audioRefs.current[activeSongIndex].current) {
+      const audioElement = audioRefs.current[activeSongIndex].current;
+      const minutes = Math.floor(audioElement.currentTime / 60);
+      const seconds = Math.floor(audioElement.currentTime % 60);
+      setSongTime(`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+    }
+  };
+
+  const handlePauseSong = (index) => {
+    if (audioRefs.current[index].current !== null) {
+      audioRefs.current[index].current.pause();
+      setIconPlay('block');
+      setIconPause('none');
+      setActiveSongIndex(null);
+      audioRefs.current[index].current.removeEventListener('timeupdate', updateTime);
+    }
+  };
+
   const elementoActivo = () => {
-    setActiveIcon()
-    setPlayIcon('block')
-    setNumber('none')
-  }
+    setActiveIcon();
+    setPlayIcon('block');
+    setNumber('none');
+  };
+
   const elementoInactivo = () => {
-    setActiveIcon(null)
-    setPlayIcon('none')
-    setNumber('block')
-  }
-
- 
-  
-
+    setActiveIcon(null);
+    setPlayIcon('none');
+    setNumber('block');
+  };
 
   return (
     <>
       <div className="contenedorReproductor">
 
-            <div className="controlesDelReproductor">
-          
-            <div className='controls'>
-              <div className="iconWrapper">
-              <FaBackwardStep style={{fontSize:"24px"}}/>
-              </div> 
-            <div className="iconWrapper">
-            <FaPlay onClick={()=>{reproducirCancion()}} style={{display:iconPlay}}/>
-            <FaPause onClick={()=>{pausarCancion()}} style={{display:iconPause}}/>
-            </div> 
-              <div className="iconWrapper">
-              <FaForwardStep style={{fontSize:"24px"}}/>
-              </div> 
-            </div>
+        <div className="overflowList">
+          <div className="containerList">
+            <ul>
+              {songList.length > 0 ? songList.map((item, index) => {
+                return (
+                  <li key={item.id} onMouseEnter={() => { elementoActivo(item); }} onMouseLeave={() => { elementoInactivo(null); }}>
+                    <div style={{ display: playIcon }}>
+                      <div className="playIconContainer">
+                        <FaPlay onClick={() => { handlePlaySong(index); }} style={{ display: iconPlay }} />
+                        <FaPause onClick={() => { handlePauseSong(index); }} style={{ display: iconPause }} />
+                      </div>
+                    </div>
 
-            </div>
-            
-            <div className="overflowList">
+                    <div className="itemInfo">
+                      <div style={{ display: number }}><div className='numberContainer'>{item.id}</div></div>
+                      <div>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</div>
+                    </div>
 
-            <div className="containerList">
-                <ul>
-                      { 
-                        listItems.length > 0 ? listItems.map((item, i)=>{
-                        return <li key={item.id} onMouseEnter={()=>{elementoActivo(item)}} onMouseLeave={()=>{elementoInactivo(null)}} >
-                            {/* {item.id !== } */}
-                            <div style={{display:playIcon}}>
-                              <div className="playIconContainer">
-                              <FaPlay onClick={()=>{reproducirCancion()}} style={{display:iconPlay}}/>
-                              <FaPause onClick={()=>{pausarCancion()}} style={{display:iconPause}}/>
-                              </div>  
-                            </div>
-                            
-                            <div className="itemInfo">
-                               
-                            <div style={{display:number}}><div className='numberContainer'>{item.id}</div></div>
-
-                            <div>{item.name.charAt(0).toUpperCase()+item.name.slice(1)}</div>
-
-                            </div>  
-                            <audio src={'https://playground.4geeks.com/apis/fake/sound/'+item.url} ref={audioRef} ></audio>
-                            <p>{songTime}</p>
-                            </li> }) 
-                        : 
-                        "tu lista esta vacia" 
-                      }
-                </ul>
-            </div>
-
-            </div>
-
+                    <audio src={`https://playground.4geeks.com/apis/fake/sound/${item.url}`} ref={audioRefs.current[index]}></audio>
+                    <p>{songTime}</p>
+                  </li>
+                );
+              }) : 'Tu lista está vacía'}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
 export default App;
-
-
-/* { 
-  listItems.length > 0 ? listItems.map((item, i)=>{
-  return <li key={i} onMouseEnter={()=>{setPlayIcon('block'),setNumber('none')}} onMouseLeave={()=>{setPlayIcon('none'),setNumber('block')}} >
-      <div style={{display:playIcon}} > <div className="playIconContainer" > <FaPlay onClick={()=>{audioRef.current.play()}}/></div> </div>
-      <div className="itemInfo"> 
-      <div style={{display:number}}> <div className='numberContainer'>{item.id}</div></div> 
-      <div>{item.name.charAt(0).toUpperCase()+item.name.slice(1)}</div> 
-      </div>
-      <audio src={"https://playground.4geeks.com/apis/fake/sound/"+item.url}  ref={audioRef} controls ></audio> 
-
-      <p>00:00</p>
-      </li> }) 
-  : 
-  "tu lista esta vacia" 
-} */
-
-
